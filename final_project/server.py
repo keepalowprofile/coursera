46ce1dc4dec7f867c57b9371ea842ca0a3e2fc41
keepalowprofile <marcucosmin@yahoo.com>
2023-07-04 09:53:21 -0400

    Final changes

---
+++ final_project/server.py
@@ -0,0 +1,26 @@
+import machinetranslation
+from machinetranslation.translator import english_to_french
+from machinetranslation.translator import french_to_english
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/englishToFrench', methods=['POST'])
+def translate_english_to_french():
+    english_text = request.form['englishText']
+    english_translated_text = english_to_french(english_text)
+    return english_translated_text
+
+@app.route('/frenchToEnglish', methods=['POST'])
+def translate_french_to_english():
+    french_text = request.form['englishText']
+    french_translated_text = french_to_english(french_text)
+    return french_translated_text
+
+if __name__ == '__main__':
+    app.run()
